08d4b0a06a706d3d8a13ca15204434b2b5ba5a11
Agasthya23 <agasthya30@gmail.com>
2025-02-20 08:26:54 +0530

    Update product metrics query

--- performance_metrics_scripts/product_metrics.sql
+++ performance_metrics_scripts/product_metrics.sql
@@ -10,8 +10,25 @@ FROM sales_data
 GROUP BY product_name
 ORDER BY total_quantity DESC;
 
--- Most returned product
+-- Most returned product by qty
 SELECT product_name, SUM(CASE WHEN Returned = 'Yes' THEN quantity ELSE 0 END) AS total_quantity_returned
 FROM sales_data
 GROUP BY product_name
 ORDER BY total_quantity_returned DESC
+
+--most returned products by transaction frequency
+SELECT product_name, COUNT(CASE WHEN Returned = 'Yes' THEN 1 END) AS total_returns
+FROM sales_data
+GROUP BY product_name
+ORDER BY total_returns DESC;
+
+-- Product with the highest loyalty points
+SELECT 
+    product_name,
+    SUM(CASE WHEN loyalty_points > 0 THEN loyalty_points ELSE 0 END) AS total_loyalty_points_earned,
+    COUNT(CASE WHEN loyalty_points > 0 THEN 1 END) AS total_transactions_with_loyalty
+FROM sales_data
+GROUP BY product_name
+ORDER BY total_loyalty_points_earned DESC;
+
+--Product th
